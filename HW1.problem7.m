a3e8d3894e3ea900f2b5d2ed299971a158636d97
Aria-Cardinale <aria.i.cardinale@gmail.com>
2026-01-23 17:38:45 -0700

    HW1.problem7.e

--- HW1.problem7.m
+++ HW1.problem7.m
@@ -1,43 +1,37 @@
-set.seed(123)  # for reproducibility
+set.seed(123)
 
-# values of n
 k_vals <- 2:10
 n_vals <- 2^k_vals
+Nsim <- 1e6
 
-Nsim <- 1e6  # number of Monte Carlo samples
-
-# function to estimate skewness
 estimate_skewness <- function(Y) {
   mean((Y - mean(Y))^3) / sd(Y)
 }
 
-# store results
 skew_sim <- numeric(length(n_vals))
 skew_theory <- n_vals^(-1/2)
 
-# main loop
 for (j in seq_along(n_vals)) {
   n <- n_vals[j]
   
-  # generate Poisson samples: Nsim x n matrix
-  X <- matrix(rpois(Nsim * n, lambda = 1), nrow = Nsim)
+  # Directly simulate the sum using Poisson additivity
+  S_n <- rpois(Nsim, lambda = n)
   
-  # compute Yn
-  Y_n <- (rowSums(X) - n) / sqrt(n)
+  # Normalized variable
+  Y_n <- (S_n - n) / sqrt(n)
   
-  # estimate skewness
   skew_sim[j] <- estimate_skewness(Y_n)
   
-  cat("Done n =", n, "\n")
+  cat("Finished n =", n, "\n")
 }
 
-# log-log plot
+# Log-log plot
 plot(n_vals, skew_sim,
      log = "xy",
      pch = 19,
      xlab = "n",
      ylab = "Skewness",
-     main = "Skewness of Yn: Simulation vs Theory")
+     main = "Skewness of Yn")
 
 lines(n_vals, skew_theory, lwd = 2)
 
